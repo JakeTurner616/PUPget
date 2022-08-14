d1cabf65ef1f2f339c2e4427578658bc27b62054
JakeTurner616 <jake@serverboi.org>
2022-08-14 05:47:57 -0600

    Update PUPgetBackend.sh

--- PUPgetBackend.sh
+++ PUPgetBackend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#The only difference with this branch is the DIR env variable is to be set rather than assumed:
 DIR="/dir/goes/here"
 curl https://www.playstation.com/en-us/support/hardware/ps3/system-software/ > source.txt
 sed -n "/data-uuid/,/div class/p" source.txt > newsource.txt && sed -i '2,$d' newsource.txt && grep -oP "http://\K[^']+" newsource.txt > newsource1.txt && grep -oP '^.*"' newsource1.txt > newsource2.txt 
