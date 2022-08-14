ddab4fa3eea0f90d006b535c55a3acde12e32bfc
JakeTurner616 <88468353+JakeTurner616@users.noreply.github.com>
2022-08-14 01:39:12 -0600

    Update PUPget.sh

--- PUPget.sh
+++ PUPget.sh
@@ -3,3 +3,7 @@ curl https://www.playstation.com/en-us/support/hardware/ps3/system-software/ > s
 sed -n "/data-uuid/,/div class/p" source.txt > newsource.txt && sed -i '2,$d' newsource.txt && grep -oP "http://\K[^']+" newsource.txt > newsource1.txt && grep -oP '^.*"' newsource1.txt > newsource2.txt 
 PUPURL=$(sed -r 's/.{204}$//' newsource2.txt)
 wget "$PUPURL" $(pwd)
+rm -r source.txt
+rm -r newsource.txt
+rm -r newsource1.txt
+rm -r newsource2.txt 
