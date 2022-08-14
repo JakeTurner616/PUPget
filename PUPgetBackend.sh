979b5779a1f9e238da33274f5f371fbbc6634033
JakeTurner616 <jake@serverboi.org>
2022-08-14 06:10:18 -0600

    Update PUPgetBackend.sh

--- PUPgetBackend.sh
+++ PUPgetBackend.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #The only difference with this branch is the DIR env variable is to be set rather than assumed:
-DIR="/dir/goes/here"
+DIR="/var/www/html/dir_goes_here"
 curl https://www.playstation.com/en-us/support/hardware/ps3/system-software/ > source.txt
-sed -n "/data-uuid/,/div class/p" source.txt > newsource.txt && sed -i '2,$d' newsource.txt && grep -oP "http://\K[^']+" newsource.txt > newsource1.txt && grep -oP '^.*"' newsource1.txt > newsource2.txt 
+sed -n "/data-uuid/,/div class/p" source.txt > newsource.txt && sed -i '2,$d' newsource.txt && grep -oP "http://\K[^']+" newsource.txt > newsource1.txt && grep -oP '^.*"' newsource1.txt > newsource2.txt
 PUPURL=$(sed -r 's/.{204}$//' newsource2.txt)
 wget "$PUPURL" -P "$DIR"
 rm -r source.txt
 rm -r newsource.txt
 rm -r newsource1.txt
-rm -r newsource2.txt 
+rm -r newsource2.txt
+rm -r "$DIR" && mv "$DIR"/PS3UPDAT.PUP.1 "$DIR"/PS3UPDAT.PUP
